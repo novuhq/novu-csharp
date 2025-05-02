aef2eb6665225acfb0d29f1264418a6784c29dfa
Himanshu Garg <garg_himanshu@outlook.com>
2025-05-02 14:33:52 +0530

    fix: update NovuCustomHook to improve code clarity and add System.Net.Http package reference

--- src/Novu/Hooks/NovuCustomHook.cs
+++ src/Novu/Hooks/NovuCustomHook.cs
@@ -1,21 +1,22 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Novu.Utils;
+using Novu.Models.Components;
+
 namespace Novu.Hooks
 {
-    using Novu.Utils;
-    using Novu.Models.Components;
-    using System.Net.Http.Headers;
-
     public class NovuCustomHook : IBeforeRequestHook
     {
         public Task<HttpRequestMessage> BeforeRequestAsync(BeforeRequestContext hookCtx, HttpRequestMessage request)
         {
-            // Retrieve authorization header
             var authHeader = request.Headers.Authorization;
             if (authHeader != null && !string.IsNullOrEmpty(authHeader.Parameter))
             {
                 request.Headers.Authorization = new AuthenticationHeaderValue("ApiKey", authHeader.Parameter);
             }
 
-            // Add an idempotency key to the header if it is not already present
             if (!request.Headers.Contains("idempotency-key"))
             {
                 request.Headers.Add("idempotency-key", Guid.NewGuid().ToString());
@@ -23,12 +24,5 @@ namespace Novu.Hooks
 
             return Task.FromResult(request);
         }
-
-        // TODO: Implement AfterSuccessAsync if needed
-        // public Task<HttpResponseMessage> AfterSuccessAsync(AfterSuccessContext hookCtx, HttpResponseMessage response)
-        // {
-        //     // Modify the response object before deserialization or throw an exception to stop the response from being returned
-        //     return Task.FromResult(response);
-        // }
     }
 }
